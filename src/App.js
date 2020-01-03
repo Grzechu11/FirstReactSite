029a19d9f2ec0776295d803b40b05c41251b2c0f
Grzegorz Dworak <grzesiekdwg@gmail.com>
2020-01-03 11:50:49 +0100

    SPA, routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Sidebar from './Components/Sidebar/sidebar'
+import Home from "./Components/Home/home";
+import Team from "./Components/Team/team";
+import SpeechToText from "./Components/SpeechToText/speechtotext";
+import HomeIcon from "@material-ui/icons/Home";
+import AccountTree from "@material-ui/icons/AccountTree";
+import EmojiPeople from "@material-ui/icons/EmojiPeople";
+import RecordVoiceOver from "@material-ui/icons/RecordVoiceOver";
+
+function onClick(e, item) {
+  window.alert(JSON.stringify(item, null, 2));
+}
+
+const items = [
+  {
+    name: "home",
+    label: "Home",
+    navLink: "/",
+    component: Home,
+    isExact: true,
+    Icon: HomeIcon
+  },
+  {
+    name: "speechtotext",
+    label: "Speech to text",
+    navLink: "/speechtotext",
+    component: SpeechToText,
+    Icon: AccountTree,
+    items: [
+      {
+        name: "audiototext",
+        label: "Audio to text",
+        Icon: RecordVoiceOver
+      }
+    ]
+  },
+  "divider",
+  {
+    name: "team",
+    label: "Team",
+    navLink: "/team",
+    component: Team,
+    Icon: EmojiPeople
+  }
+];
+
+function RouterRoute({navLink, isExact, component}){
+  if(!navLink){
+    return null;
+  }
+  if(isExact){
+    return (
+      <Route exact path={navLink} component={component} />
+    );
+  }
+  return (
+    <Route path={navLink} component={component} />
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <HashRouter>
+      <div className="app">
+        <Sidebar items={items} />
+        <div className="app-content">
+          <h1>Simple SPA</h1>
+          {items
+            .filter((subItem) => subItem.navLink)
+              .map((subItem, index) => (
+                <React.Fragment key={`${subItem.name}${index}`}>
+                  <RouterRoute navLink={subItem.navLink} isExact={subItem.isExact} component={subItem.component} />
+                </React.Fragment>
+              ))}
+        </div>
       </div>
-    </div>
+    </HashRouter>
   );
 }
 
---
+++ src/Components/SpeechToText/speechtotext.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import './speechtotext.css';
+ 
+class SpeechToText extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Speech To Text</h2>
+      </div>
+    );
+  }
+}
+ 
+export default SpeechToText;
